d7c4c4ca31a583bc319a2db4666982c6f1e3d499
Arshan A Shaik <arshanshaik1199@gmail.com>
2025-07-27 19:43:16 +0530

    cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='from eda to CI/CD and deployment with experiment tracking. Chrome plugin that helps you with quick sentiment analysis of comment section ! Complete inhouse ML model (not a LLM Wrapper)!!!',
+    author='Arshan',
+    license='BSD-3',
+)
